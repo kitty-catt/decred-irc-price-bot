2de214f3b17bf5ac550fe04863caef06172abc58
LifeMushroom <mathmaster01@gmail.com>
2014-10-25 13:19:46 -0400

    add help module

--- modules/help.js
+++ modules/help.js
@@ -3,7 +3,8 @@ var os = require("os");
 var settings = require("../settings.js").settings;
 
 exports.module = function() {
-	this.onCommand_about = function(nick, command) {
+	this.onCommand_help = function(nick, command) {
 		this.channel.say(settings.globalNick + " by Dirbaio, Nina, and LifeMushroom. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
+		this.channel.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/COMMANDS.md");
 	}
 }
