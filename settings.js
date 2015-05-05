3fad511213dc686ecea9ad9b19552b84a8320dba
AlphaTech <alphat3ch@icloud.com>
2015-05-05 18:32:26 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ exports.settings = {
 	// prefix of most commands for bot
 	defaultCommandPrefix: "!",
         // The IRC nick of the person actually running the bot
-        globalowner: "AlphaTech",
+        globalowner: "UnconfiguredBotOwner",
 	// Set to true to enable notifications when an update is available (checks when the bot starts and will notify via IRC to the owner. The tell module must be enabled)
 	updatenotify: "true",
 
