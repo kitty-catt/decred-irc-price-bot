96c8249f81a476cc705af8f82255306b11d08568
AlphaTech <AlphaT3ch@icloud.com>
2015-06-18 16:11:45 -0400

    Don't greet yourself

--- modules/welcome.js
+++ modules/welcome.js
@@ -1,11 +1,15 @@
 "use strict"
+var settings = require("../config.js").settings;
+
 // Welcomes the user when they join
 exports.module = function() {
 	this.onUserJoin = function(user) {
-		var message = this.settings.message;
-		if(!message)
-			message = "Welcome back, %!";
-		message = message.replace("%", user);
-		this.channel.say(message);
+		if (settings.globalNick != user) {
+			var message = this.settings.message;
+			if(!message)
+				message = "Welcome back, %!";
+			message = message.replace("%", user);
+			this.channel.say(message);
+		}
 	}
 }
