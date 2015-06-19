52c132cecca4c9a123af99523d04f12315763763
AlphaTech <AlphaT3ch@icloud.com>
2015-06-19 04:48:28 -0400

    Update wikipedia.js

--- modules/wikipedia.js
+++ modules/wikipedia.js
@@ -1,17 +1,23 @@
 "use strict"
 
-var wiki = require ('wtf_wikipedia');
+var wikipedia = require ('wtf_wikipedia');
 
 exports.module = function() {
 	this.onCommand_wiki = function(nick, args) {
 		var chan = this.channel;
 		if(args.trim() != "") {
-			wikipedia.from_api(args, "en", function(markup){                                                                                                               
-  				chan.say(wikipedia.plaintext(markup).substring(0,100) + "...");                                                                                                 
+			wikipedia.from_api(args, "en", function(markup){
+  				var text = wikipedia.plaintext(markup).substring(0,100) + "...";
+				if (text != "...") {
+					chan.say(text);
+					chan.say("https://en.wikipedia.org/w/index.php?title=" + encodeURIComponent(args));
+				} else {
+					chan.say("No page found");
+				}
 			});
-			chan.say("https://en.wikipedia.org/w/index.php?title=" + encodeURIComponent(args));
 		} else {
 			chan.say("You're doing it wrong.\nUsage: wiki query");
 		}
 	}
 }
+
