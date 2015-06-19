adafcbb0900524ec47c6c7fe7217d126088794d2
AlphaTech <AlphaT3ch@icloud.com>
2015-06-19 02:23:33 -0400

    Auto-Updater

--- config.js
+++ config.js
@@ -9,10 +9,6 @@ exports.settings = {
 	globalRealName: "Node.js IRC Bot",
 	// prefix of most commands for bot
 	defaultCommandPrefix: "!",
-	// The IRC nick of the person actually running the bot
-	globalowner: "UnconfiguredBotOwner",
-	// Set to true to enable notifications when an update is available (checks when the bot starts and will notify via IRC to the owner. The tell module must be enabled)
-	updatenotify: "true",
 
 	// These modules will be loaded in all channels.
 	globalServModules: {
