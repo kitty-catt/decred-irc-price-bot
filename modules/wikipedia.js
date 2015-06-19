8d3f3e22b68e5c523424e20d6a8f9cf8b2750491
AlphaTech <alphat3ch@icloud.com>
2015-06-19 04:36:33 -0400

    Update wikipedia.js

--- modules/wikipedia.js
+++ modules/wikipedia.js
@@ -1,9 +1,17 @@
 "use strict"
 
-var wiki = require ('sk-wikipedia');
+var wiki = require ('wtf_wikipedia');
 
 exports.module = function() {
-	this.onCommand_ping = function(nick, command) {
-		this.channel.say(wiki);
+	this.onCommand_wiki = function(nick, args) {
+		var chan = this.channel;
+		if(args.trim() != "") {
+			wikipedia.from_api(args, "en", function(markup){                                                                                                               
+  				chan.say(wikipedia.plaintext(markup).substring(0,100) + "...");                                                                                                 
+			});
+			chan.say("https://en.wikipedia.org/w/index.php?title=" + encodeURIComponent(args));
+		} else {
+			chan.say("You're doing it wrong.\nUsage: wiki query");
+		}
 	}
 }
