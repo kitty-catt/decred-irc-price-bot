f5db232677573be4dd4c0743369c58a3741b462f
LifeMushroom <mathmaster01@gmail.com>
2015-06-21 17:51:38 -0400

    I think AlphaTech can now consider himself a developer! :)

--- modules/help.js
+++ modules/help.js
@@ -4,7 +4,7 @@ var settings = require("../config.js").settings;
 
 exports.module = function() {
 	this.onCommand_help = function(nick, command) {
-		this.channel.say(settings.globalNick + " by Dirbaio, Nina, and LifeMushroom. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
+		this.channel.say(settings.globalNick + " by Dirbaio, Nina, LifeMushroom, and AlphaTech. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
 		this.channel.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/Docs/COMMANDS.md");
 	}
 }
