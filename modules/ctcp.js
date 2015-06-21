2d25aa8fb6823b7d15eadff3d0da61f50da60e44
LifeMushroom <mathmaster01@gmail.com>
2015-06-21 19:06:04 -0400

    Update ctcp.js

--- modules/ctcp.js
+++ modules/ctcp.js
@@ -8,7 +8,7 @@ exports.module = function() {
 		var st = String.fromCharCode(1);
 		var os = require("os");
 		if (message == st+"VERSION"+st) {
-			this.server.notice(user, settings.globalNick + " by Dirbaio, Nina, and LifeMushroom. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ") https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot");
+			this.server.notice(user, settings.globalNick + " by Dirbaio, Nina, LifeMushroom, and AlphaTech. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ") https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot");
 		} else if (message == st+"TIME"+st) {
 			this.server.notice(user, (new Date()).toString());
 		} else if (message == st+"PING"+st) {
