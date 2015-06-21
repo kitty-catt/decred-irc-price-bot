e2c756e4052e2167006a140ef59f8af7a4431550
LifeMushroom <mathmaster01@gmail.com>
2015-06-21 16:57:51 -0400

    update link

--- modules/help.js
+++ modules/help.js
@@ -5,6 +5,6 @@ var settings = require("../config.js").settings;
 exports.module = function() {
 	this.onCommand_help = function(nick, command) {
 		this.channel.say(settings.globalNick + " by Dirbaio, Nina, and LifeMushroom. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
-		this.channel.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/COMMANDS.md");
+		this.channel.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/Docs/COMMANDS.md");
 	}
 }
