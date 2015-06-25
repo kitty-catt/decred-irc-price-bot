6e6df32cab8502293839c79b77de6d7864f01dd5
AlphaTech <alphat3ch@icloud.com>
2015-06-25 16:01:58 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,38 @@
 var gulp = require('gulp'); 
 var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var stripDebug = require('gulp-strip-debug');
-var uglify = require('gulp-uglify');
-var changed = require('gulp-changed');
 var markdown = require('gulp-markdown');
-var jsonFormat = require('gulp-json-format');
 
 gulp.task('default', function() {
-  gulp.src('./app.js')
+  gulp.src('./*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-	
-  gulp.src('./main.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-	
+
   gulp.src('./lib/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-	
+
   gulp.src('./modules/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-	
+
   gulp.src('./modules/misc/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-	
-  gulp.src(['./app.js'])
-    .pipe(concat('app.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/JavaScript'));
-	
-  gulp.src(['./main.js'])
-    .pipe(concat('main.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/JavaScript'));
-	
-  gulp.src(['./lib/*.js'])
-    .pipe(concat('./lib/*.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/JavaScript'));
-	
-  gulp.src(['./modules/*.js'])
-    .pipe(concat('./modules/*.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/JavaScript'));
-	
-  gulp.src(['./modules/misc/*.js'])
-    .pipe(concat('./modules/misc/*.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/JavaScript'));
-	
-  gulp.src('*.md')
+
+  gulp.src('./*.md')
     .pipe(markdown())
     .pipe(gulp.dest('./build/markdown'));
-	
+
   gulp.src('./Docs/*.md')
     .pipe(markdown())
     .pipe(gulp.dest('./build/markdown'));
-	
-  gulp.src('*.json')
-    .pipe(jsonFormat(4))
-    .pipe(gulp.dest('./build/json'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./*.js', ['default']);
+    gulp.watch('./*.md', ['default']);
+    gulp.watch('./Docs/*.md', ['default']);
+    gulp.watch('./lib/*.js', ['default']);
+    gulp.watch('./modules/*.js', ['default']);
+    gulp.watch('./modules/misc/*.js', ['default']);
 });
