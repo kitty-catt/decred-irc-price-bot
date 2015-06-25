531620677c4b3a896e54ef7096c3f4c396fc1c0c
AlphaTech <alphat3ch@icloud.com>
2015-06-25 07:11:33 -0400

    Create vgd.js

---
+++ modules/vgd.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var settings = require('../config').settings;
+var vgd = require('vgd');
+
+exports.module = function() { 
+	this.onCommand_vgd = function(user, args) { 
+		if(args.trim() != "") {
+			var chan = this.channel;
+			vgd.shorten(args.split(" ")[0], function(res) {
+        chan.say(res);
+      });
+		} else {
+			this.channel.say("You're doing it wrong.\nUsage: " + settings.defaultCommandTrigger + "vgd <URL>");
+		}
+	} 
+}
