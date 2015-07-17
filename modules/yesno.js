58451c589b8109cab8be9b06a7685b857d94efb1
AlphaTech <alphat3ch@icloud.com>
2015-07-16 23:44:37 -0400

    Pictures make everything better!

--- modules/yesno.js
+++ modules/yesno.js
@@ -1,13 +1,15 @@
 "use strict";
 
+var request = require('request');
+
 exports.module = function() {
 	this.onMessage = function(user, message) {
 		if (message.toLowerCase().indexOf("y/n") != -1) {
-			if (Math.random() > 0.5) {
-				this.channel.say(user + ": Yes.");
-			} else {
-				this.channel.say(user + ": No.");
-			}
+			var chan = this.channel;
+			request("http://yesno.wtf/api", function(err, res, body) {
+				var data = JSON.parse(body);
+				chan.say(user + ": " + data.answer.charAt(0).toUpperCase() + data.answer.slice(1) + "! " + data.image);
+			});
 		}
 	};
 };
