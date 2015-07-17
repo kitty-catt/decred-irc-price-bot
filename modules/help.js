ed5abf4a2d700832ca57c7a429622e806c8458ba
AlphaTech <alphat3ch@icloud.com>
2015-07-17 10:07:08 -0400

    Version in the help command

--- modules/help.js
+++ modules/help.js
@@ -1,10 +1,18 @@
 "use strict";
 var os = require("os");
+var fs = require('fs');
 var settings = require("../config.js").settings;
 
 exports.module = function() {
 	this.onCommand_help = function(nick, command) {
-		this.channel.say(settings.globalNick + " by Dirbaio, Nina, LifeMushroom, and AlphaTech. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
-		this.channel.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/Docs/COMMANDS.md");
+		var chan = this.channel;
+		fs.readFile('./package.json', 'utf-8', function(err, data) {
+			if (!err) {
+				chan.say(settings.globalNick + " v" + JSON.parse(data).version + " by Dirbaio, Nina, LifeMushroom, and AlphaTech. Running on Node.js " + process.versions.node + " (" + os.type() + " " + os.release() + " " + os.arch() + ").");
+				chan.say("For a list of available commands, check https://github.com/LifeMushroom/Modular-Node.js-IRC-Bot/blob/master/Docs/COMMANDS.md");
+			} else {
+				console.err("Error opening ./package.js... Did you delete it?")
+			}
+		});
 	};
 };
