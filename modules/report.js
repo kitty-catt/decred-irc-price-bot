7632dd716be105a336ad7b3759c35893d16c3dbb
Jesús "JeDa" Hernández <jedayoshi@gmail.com>
2015-08-05 19:55:01 -0430

    Instant reporting.
    Definitelly a lot better. :)

--- modules/report.js
+++ modules/report.js
@@ -13,20 +13,49 @@ exports.module = function() {
 			if(self.settings.allowedip != "any" && co.remoteAddress != self.settings.allowedip)
 			{
 				co.end();
+				console.log("[Warning] Killing "use strict";
+
+var net = require('net');
+
+exports.module = function() {
+	this.onModuleStart = function(settings) {
+	
+		if(!this.settings.allowedip)
+			this.settings.allowedip = "127.0.0.1";
+		
+		var self = this;
+		this.server = net.createServer(function(co) {
+			if(self.settings.allowedip != "any" && co.remoteAddress != self.settings.allowedip)
+			{
+				co.end();
 				console.log("[Warning] Killing unauthorized connection for post report plugin. Port "+self.settings.port+", IP "+co.remoteAddress);
 				return;
 			}
-
-			var re = "";
 			
 			co.on("data", function(data) {
-				re += data;
+				self.channel.say(data);
 			});
 
-			co.on('end', function() {
-				self.channel.say(re);
-				re = "";
+		});
+		
+		if(this.settings.allowedip == "127.0.0.1")
+			this.server.listen(this.settings.port, "127.0.0.1");
+		else
+			this.server.listen(this.settings.port);
+	};
+
+	this.onModuleDestroy = function() {
+		this.server.close();
+	};
+};
+ unauthorized connection for post report plugin. Port "+self.settings.port+", IP "+co.remoteAddress);
+				return;
+			}
+			
+			co.on("data", function(data) {
+				self.channel.say(data);
 			});
+
 		});
 		
 		if(this.settings.allowedip == "127.0.0.1")
