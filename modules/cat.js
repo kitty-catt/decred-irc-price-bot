5f34c3becf2c8b1a3a478bd7c404b7d15c0d7c3f
AlphaTech <alphat3ch@icloud.com>
2015-09-13 18:45:02 -0400

    Shorten Code

--- modules/cat.js
+++ modules/cat.js
@@ -8,25 +8,18 @@ exports.module = function() {
 	this.onCommand_cat = function(user, args) { 
 		var chan = this.channel;
 		if(args === "") {
-			request("http://thecatapi.com/api/images/get?format=xml", function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					var data = XML.parse(body);
-					chan.say("Cat " + data.data.images.image.id + ": " + data.data.images.image.url);
-				} else {
-					chan.say("[ERROR] " + error);
-					console.err(error);
-				}
-			});
-		} else {
-			request("http://thecatapi.com/api/images/get?format=xml&image_id=" + args.split(" ")[0], function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					var data = XML.parse(body);
-					chan.say("Cat " + data.data.images.image.id + ": " + data.data.images.image.url);
-				} else {
-					chan.say("[ERROR] " + error);
-					console.err(error);
-				}
-			});
-		}
-	} ;
+                        var rURL = "http://thecatapi.com/api/images/get?format=xml";
+                } else {
+                        var rURL = "http://thecatapi.com/api/images/get?format=xml&image_id=" + args.split(" ")[0];
+                }
+	        request($rURL, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var data = XML.parse(body);
+				chan.say("Cat " + data.data.images.image.id + ": " + data.data.images.image.url);
+			} else {
+				chan.say("[ERROR] " + error);
+				console.err(error);
+			}
+		});
+	};
 };
