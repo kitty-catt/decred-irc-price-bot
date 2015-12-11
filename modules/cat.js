6dabc48e13fbe3c8af6a062ae590b2dacb676a9a
AlphaTech <alphat3ch@icloud.com>
2015-12-11 18:23:22 -0500

    Update cat.js

--- modules/cat.js
+++ modules/cat.js
@@ -6,10 +6,9 @@ var XML = require('pixl-xml');
 exports.module = function() { 
 	this.onCommand_cat = function(user, args) { 
 		var chan = this.channel;
-		if(args === "") {
-                        var rURL = "http://thecatapi.com/api/images/get?format=xml";
-                } else {
-                        var rURL = "http://thecatapi.com/api/images/get?format=xml&image_id=" + args.split(" ")[0];
+		var rURL = "http://thecatapi.com/api/images/get?format=xml";
+		if(args != "") {
+                        var rURL = $rURL + "&image_id=" + args.split(" ")[0];
                 }
 	        request($rURL, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
